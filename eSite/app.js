14e0a4ea24d7cbf44f51b912a70df81aee73b872
arefinMilen <arefin15-5279@diu.edu.bd>
2026-04-09 23:59:56 +0600

    update main js file path and exports

--- eSite/app.js
+++ eSite/app.js
@@ -1,9 +1,11 @@
+//core modules
+const path = require("path");
+//external modules
 const express = require("express");
-
 //local routers
 const userRouter = require("./routes/userRouter");
 const hostRouter = require("./routes/hostRouter");
-
+const rootDir = require("./utils/pathUtil");
 const app = express();
 
 app.use(express.urlencoded());
@@ -11,9 +13,7 @@ app.use(userRouter);
 app.use(hostRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send(`<h1>404 Not Found</h1>
-    <a href="/">Go to Home</a>
-    `);
+  res.status(404).sendFile(path.join(rootDir,"views", '404.html'));
 })
 const PORT = 3000;
 app.listen(PORT, () => {
