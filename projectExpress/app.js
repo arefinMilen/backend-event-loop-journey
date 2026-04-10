021f560663d6c7ab50e2e44cc671b0748af66cae
arefinMilen <arefin15-5279@diu.edu.bd>
2026-04-10 18:14:43 +0600

    imports all routes and utils path

--- projectExpress/app.js
+++ projectExpress/app.js
@@ -1,36 +1,15 @@
+const path = require("path");
 const express = require("express");
+const homeRouter = require("./routes/home");
+const contactRouter = require("./routes/contact");
+
 
 const app = express();
+app.use(homeRouter);
+app.use(contactRouter);
 
-app.use((req, res, next) => {
-  console.log("first middleware", req.url, req.method);
-  next();
-})
-app.use((req, res, next) => {
-  console.log("second middleware", req.url, req.method);
-  next();
-})
-// app.use((req, res, next) => {
-//   console.log("third middleware", req.url, req.method);
-//   res.send(`<h1>Welcome to my Express server</h1>`);
-// })
-app.get("/",(req,res,next)=> {
-  console.log("GET request received for root path",req.url, req.method);
-  res.send(`<h1>Welcome to my Express server</h1>`);
-})
-app.get("/contact-us",(req,res,next)=> {
-  console.log("GET request received for /contact-us", req.url, req.method);
-  res.send(`<form action="/contact-us" method="POST">
-    <input type="text" name="name" placeholder="Enter your name" required><br><br>
-    <input type="email" name="email" placeholder="Enter your email" required><br><br>
-    <textarea name="message" placeholder="Enter your message" required></textarea><br><br>
-    <input type="submit" value="Submit">
-  </form>`);
-})
-app.post("/contact-us",(req,res,next)=> {
-  console.log("POST request received for /contact-us", req.url, req.method);
-  res.send(`<h1>Thank you for contacting us!</h1>`);
-})
+
+
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
