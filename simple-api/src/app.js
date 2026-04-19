9933e2ff562a35fdd4be9344826244e46a0d93db
arefinMilen <arefin15-5279@diu.edu.bd>
2026-04-19 16:17:20 +0600

    creted http methods with get post delete patch

--- simple-api/src/app.js
+++ simple-api/src/app.js
@@ -1,39 +1,80 @@
 const express = require('express');
+const noteModel = require("./models/note.model")
 const app = express();
+
+//middleware  
 app.use(express.json());
 
-const notes = [];
-// title, description ,post ,notes
-app.post('/notes', (req, res)=> {
-  notes.push(req.body);
-  res.status(201).json({message: 'Note added successfully', note: req.body});
-})
+app.post('/notes', async (req,res)=>{
+  const data = req.body;
+  await noteModel.create({
+  title:data.title,
+  description:data.description
+  })
 
-app.get('/notes', (req, res) => {
+  res.status(201).json({
+    message: "note created"
+  })
+
+})
+// title, description ,post ,notes
+// app.post('/notes', (req, res)=> {
+//   notes.push(req.body);
+//   res.status(201).json({message: 'Note added successfully', note: req.body});
+// })
+
+app.get('/notes', async (req, res) => {
+
+  const notes = await noteModel.find({
+
+  })
   
   res.status(200).json({
-    message: 'Notes retrieved successfully',
+    message: 'Notes fetch successfully',
     notes: notes
   })
 })
 
-app.delete('/notes/:id', (req, res) =>{
+app.delete('/notes/:id', async (req,res) => {
   const id = req.params.id;
-  delete notes[id];
+  await noteModel.findOneAndDelete({
+    _id:id
+  })
+res.status(200).json({
+  message:"Notes deleted succesfully"
+})
+})
+
+
+
+// app.delete('/notes/:id', (req, res) =>{
+//   const id = req.params.id;
+//   delete notes[id];
+//   res.status(200).json({
+//     message: `Note with id ${id} deleted successfully`
+//   })
+// })
+
+app.patch('/notes/:id', async (req,res) =>{
+  const id = req.params.id;
+  description = req.body.description
+  await noteModel.findOneAndUpdate({_id:id},{description: description})
+
   res.status(200).json({
-    message: `Note with id ${id} deleted successfully`
+    message:"Note Updated successfully"
   })
 })
 
-app.patch('/notes/:id', (req, res) => {
-  const id = req.params.id;
-  const description = req.body.description;
-  notes[ id ].description = description;
+// app.patch('/notes/:id', (req, res) => {
+//   const id = req.params.id;
+//   const description = req.body.description;
+//   notes[ id ].description = description;
   
-  res.status(200).json({
-    message: `Note with id ${id} updated
-    successfully`
+//   res.status(200).json({
+//     message: `Note with id ${id} updated
+//     successfully`
 
-  })
-})
+//   })
+// })
+
 module.exports = app;
