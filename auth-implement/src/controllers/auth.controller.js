07fe8655aca325e7e7da788db68c69f8d5f59cf2
arefinMilen <arefin15-5279@diu.edu.bd>
2026-04-23 16:17:42 +0600

    store token in cookies storage

--- auth-implement/src/controllers/auth.controller.js
+++ auth-implement/src/controllers/auth.controller.js
@@ -4,6 +4,16 @@ const jwt = require('jsonwebtoken')
 
 const registerUser = async (req,res) => {
   const {username,email,password} = req.body
+
+  const isUserAlreadyExists = await userModel.findOne({
+    email
+  })
+  
+  if(isUserAlreadyExists) {
+    return res.status(409).json({
+        message:"User already exists"
+    })
+  }
   
   const user = await userModel.create({
     username,email,password
@@ -12,10 +22,10 @@ const token = jwt.sign({
   id:user._id
 },process.env.JWT_SECRET)
 
+res.cookie("token",token)
 res.status(201).json({
   message:"user registered succesfully",
-  user,
-  token
+  user
 })
 
 }
