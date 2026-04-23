117e0df82f38b95e4ac4cbacb4533dbe4eb9e755
arefinMilen <arefin15-5279@diu.edu.bd>
2026-04-23 16:20:20 +0600

    add middleware in app js and install npm packages

--- auth-implement/src/app.js
+++ auth-implement/src/app.js
@@ -1,11 +1,17 @@
 
 const express = require("express")
 const authRoute = require('../src/routes/auth.route')
+const postRoute = require('../src/routes/post.route')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 //middleware
 app.use(express.json());
+app.use(cookieParser());
+
+// routes
 app.use('/api/auth',authRoute)
+app.use('/api/post',postRoute)
 
 
 
