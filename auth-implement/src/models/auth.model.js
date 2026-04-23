aea8a7a310b68eeb5af773506fd5cd16713c5c98
arefinMilen <arefin15-5279@diu.edu.bd>
2026-04-23 16:15:04 +0600

    update user email schema for unique user registration

--- auth-implement/src/models/auth.model.js
+++ auth-implement/src/models/auth.model.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: String,
-  email: String,
+  email: {
+       type: String,
+       unique: true
+  },
   password: String
 })
 
