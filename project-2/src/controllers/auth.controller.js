ffa0b90e8a40b9f95240542a2e74fdd772ff9e63
arefinMilen <arefin15-5279@diu.edu.bd>
2026-04-24 17:01:31 +0600

    implement user authorization and hasing the password with send the token in the cookies

---
+++ project-2/src/controllers/auth.controller.js
@@ -0,0 +1,47 @@
+const userModel = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs'); 
+
+
+const registerUser = async(req,res)=> {
+  const {username,email,password,role='user'} = req.body
+  const isUserAlreadyExists = await userModel.findOne({
+    $or: [
+      {username},
+      {email}
+    ]
+  })
+
+  if(isUserAlreadyExists){
+   return res.status(409).json({
+      message:"user already exists"
+    })
+  }
+
+  const hash = await bcrypt.hash(password, 10)
+  const user = await userModel.create({
+    username,
+    email,
+    password:hash,
+    role
+  })
+
+  const token = jwt.sign({
+    id:user._id,
+    role:user.role
+  }, process.env.JWT_SECRET)
+
+  res.cookie("token",token)
+  
+  res.status(201).json({
+    message:"user registration succesfully",
+    user:{
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    }
+  })
+}
+
+module.exports = {registerUser}
