b816ff79167aa78a851f55219c589fb0a6185a94
arefinMilen <arefin15-5279@diu.edu.bd>
2026-04-24 16:58:22 +0600

    db connected succesfully

---
+++ project-2/src/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const connectDB = async ()=> {
+  try{
+
+  await mongoose.connect(process.env.MONGO_URI)
+  console.log("db is connected succesfuly")
+  } catch(err){
+
+    console.log("db is not connected")
+  }
+  
+}
+
+module.exports = connectDB
